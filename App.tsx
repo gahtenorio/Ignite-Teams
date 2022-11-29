14ee1a6dc6c39e5d67fcc656d857d5b50018725b
gahtenorio <jgst25@gmail.com>
2022-11-29 07:36:54 -0300

    config fonts and component loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { Groups } from '@screens/Groups';
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+
+import { Loading } from '@components/Loading';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { Groups } from '@screens/Groups';
+
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
